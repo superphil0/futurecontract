f350d980367ae7c9f9636e2af12cf24b15f634eb
Philipp Omenitsch <superphil0@users.noreply.github.com>
2017-05-28 18:31:34 +0200

    Added comments
    comments and send ether to seller

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.4.9;
 import "std.sol";
 
 contract Contract is owned {
-
+// an offer consists of the adress of the seller, an excercise date, an idication if the contract has been sold,
+// the amount and price of the goods offered (i.e. 1 ton of pumpkin)
   struct Offer {
     uint exercise_date;
     bool sold;
@@ -12,20 +13,28 @@ contract Contract is owned {
     address buyer;
   }
   
+  // This holds all offers which are made
   Offer[] public offers;
+  
+  // This function can be called by the Farmer who sells pumpkins, he can place an offer with it
   function makeOffer(uint amount, uint exercise_date, uint price) {
     offers.push(Offer({
       amount: amount,
       exercise_date: exercise_date,
       price: price,
       sold: false,
-      buyer: msg.sender
+      buyer: 0,
+      seller: msg.sender
     }));
     
   }
   
+  // this function can be called by a potential buyer, he has to provide the id of the offer he wants to buy
+  // additionally he has to include the correct price in form of ethereum
+  // as always the caller has to make sure to provide enough gas
   function makeBid(uint offerId) payable
   {
+  // check if the offer is still valid and if enough ethereum was provided
     if(now > offers[offerId].exercise_date || 
        offers[offerId].sold ||
        offers[offerId].price > msg.value)
@@ -35,6 +44,10 @@ contract Contract is owned {
  
     offers[offerId].buyer = msg.sender;
     offers[offerId].sold = true;
+    
+    // actually send the money
+    offers[offerId].seller.transfer(offers[offerId].price);
+    
   }
   
     // Fallback function - Called if other functions don't match call or
